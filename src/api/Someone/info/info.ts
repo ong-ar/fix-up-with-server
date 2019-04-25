360f6fe0cf3738cd63534365c07aadaabd33f045
ong-ar <reclusis@gmail.com>
2019-04-25 18:58:55 +0900

    someoneReply 추가 작업

--- src/api/Someone/info/info.ts
+++ src/api/Someone/info/info.ts
@@ -3,10 +3,12 @@ import {
   SomeOne,
   User,
   SomeOnePhoto,
+  Reply,
 } from "../../../../prisma/prisma-client";
 import {
   SomeOneProfileQueryArgs,
   SomeOnePhotoQueryArgs,
+  SomeOneReplyQueryArgs,
 } from "../../../types/graph";
 import { isAuthenticated } from "../../../middlewares";
 
@@ -52,12 +54,46 @@ export default {
         return null;
       }
     },
+    SomeOneReply: async (
+      _,
+      args: SomeOneReplyQueryArgs,
+      { request },
+    ): Promise<Reply[] | null> => {
+      isAuthenticated(request);
+      const {
+        user: { id: user },
+      } = request;
+
+      const { id } = args;
+
+      if (isAccess(id, user)) {
+        const someOneReply: Reply[] = await prisma
+          .someOne({ id })
+          .reply()
+          .$fragment(ReplyFragment);
+        return someOneReply;
+      } else {
+        return null;
+      }
+    },
   },
 };
 
 interface ISomeOne extends SomeOne {
   user: User;
 }
+
+const ReplyFragment = `fragment ReplyWithUser on Reply {
+        id
+        writer {
+          id
+          email
+          password
+        }
+        content
+        createdAt
+        updatedAt
+      }`;
 
 const SomeOneFragment = `fragment SomeOneWithUser on SomeOne {
         id
